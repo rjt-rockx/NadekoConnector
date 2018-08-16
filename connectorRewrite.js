3786563a15814ec692a50507114921361fdf9601
rjt <rajju3004@gmail.com>
2018-08-16 07:54:15 +0530

    Code cleanup
    Fuck destructuring in params

--- connectorRewrite.js
+++ connectorRewrite.js
@@ -5,12 +5,11 @@ class Connector {
 
 	/**
 	 * Creates a new Connector for the specified configuration.
-	 * @param {Object} config Configuration to initialize the Connector with.
-	 * @param {String} config.dbPath Path to the database (.db)
-	 * @param {String} config.credentialsPath Path to the credentials file (.json)
+	 * @param {String} dbPath Path to the database (.db)
+	 * @param {String} credentialsPath Path to the credentials file (.json)
 	 * @param {[String]} [disabledEndpoints=[]] Endpoints to disable.
 	 */
-	constructor({ dbPath, credentialsPath }, disabledEndpoints) {
+	constructor(dbPath, credentialsPath, disabledEndpoints) {
 		this.db = new knex({
 			client: "sqlite3",
 			connection: {
@@ -19,7 +18,7 @@ class Connector {
 			useNullAsDefault: true
 		});
 		this.credentials = utils.readJson(credentialsPath);
-		this._endpoints = [];
+		this._endpoints = ["getBotInfo", "getTables", "getFields", "execSql", "getBalance", "setBalance", "createTransaction", "getTransactions", "getGuildRank", "getGlobalRank", "getGuildXp", "setGuildXp"];
 		if (!disabledEndpoints)
 			disabledEndpoints = [];
 		this._disabledEndpoints = disabledEndpoints;
@@ -50,16 +49,28 @@ class Connector {
 	}
 
 	/**
-	 * Check if the connector is initialized or not.
+	 * Get the initialization state of the connector.
 	 * @returns {Boolean} Whether the connector is initialized or not.
 	 */
 	get initialized() {
 		return this._init;
 	}
 
+	/**
+	 * Check if the connector has been initialized or not.
+	 */
 	checkInitialized() {
 		if (!this._init)
 			throw new Error("Connector not initialized. This may lead to unexpected errors.");
+	}
+
+	/**
+	 * Check if the endpoint has been disabled.
+	 */
+	checkEndpoint(endpoint) {
+		this.checkInitialized();
+		if (this._disabledEndpoints.map(endpoint => endpoint.toLowerCase()).includes(endpoint.toLowerCase()))
+			throw new Error("Endpoint disabled.");
 	}
 
 	/**
@@ -75,11 +86,10 @@ class Connector {
 
 	/**
 	 * Gets info about the bot.
-	 * @param {Object} data Empty object
-	 * @returns {Object} Info about the bot
+	 * @returns {Object} Info about the bot.
 	 */
 	async getBotInfo() {
-		this.checkInitialized();
+		this.checkEndpoint("getBotInfo");
 		let dbInfo = await this.db.first("CurrencySign", "CurrencyName", "CurrencyPluralName", "XpPerMessage", "XpMinutesTimeout").from("BotConfig");
 		let info = {
 			id: this.credentials.ClientId,
@@ -101,13 +111,10 @@ class Connector {
 
 	/**
 	 * Gets the tables present in the database.
-	 * @param {Object} data Empty object.
-	 * @returns {Object} Array of tablenames.
+	 * @returns {Object} Array of table names.
 	 */
-	async getTables({ }) {
-		this.checkInitialized();
-		if (this._disabledEndpoints.includes("getTables"))
-			throw new Error("Endpoint disabled.");
+	async getTables() {
+		this.checkEndpoint("getTables");
 		let tables = await this.db.select("name").from("sqlite_master").where({
 			type: "table"
 		}).map(table => table.name);
@@ -118,14 +125,11 @@ class Connector {
 
 	/**
 	 * Gets fields present in the specified table.
-	 * @param {Object} data Object containing the name of the table.
-	 * @param {String} data.table Name of the table.
+	 * @param {String} table Name of the table.
 	 * @returns {Object} Array of field names.
 	 */
-	async getFields({ table }) {
-		this.checkInitialized();
-		if (this._disabledEndpoints.includes("getFields"))
-			throw new Error("Endpoint disabled.");
+	async getFields(table) {
+		this.checkEndpoint("getFields");
 		let existingTables = this.getTables({}).tables;
 		if (!existingTables.includes(table))
 			throw new Error(`${table} is not present in the database. `);
@@ -137,14 +141,11 @@ class Connector {
 
 	/**
 	 * Execute a raw SQL query and return the rows.
-	 * @param {Object} data Object containing the command to execute
-	 * @param {String} data.command The SQL command to execute
+	 * @param {String} command The SQL command to execute.
 	 * @returns {Object} Result of the command, array of rows if multiple rows were affected or a single JSON object if a single row was affected.
 	 */
-	async execSql({ command }) {
-		this.checkInitialized();
-		if (this._disabledEndpoints.includes("execSql"))
-			throw new Error("Endpoint disabled.");
+	async execSql(command) {
+		this.checkEndpoint("execSql");
 		let result = await this.db.raw(command);
 		let rowsAffected = result.length;
 		if (!result)
@@ -161,14 +162,11 @@ class Connector {
 
 	/**
 	 * Get the balance of a Discord user.
-	 * @param {Object} data Object containing the user ID to get the balance of.
-	 * @param {String} data.userId User ID to get the balance of.
+	 * @param {String} userId User ID to get the balance of.
 	 * @returns {Object} Balance info about the specified user.
 	 */
-	async getBalance({ userId }) {
-		this.checkInitialized();
-		if (this._disabledEndpoints.includes("getBalance"))
-			throw new Error("Endpoint disabled.");
+	async getBalance(userId) {
+		this.checkEndpoint("getBalance");
 		let info = await this.db.raw(`select cast(UserId as text) as 'userId', CurrencyAmount as 'balance' from DiscordUser where UserId = ${userId}`);
 		if (!info)
 			throw new Error("User not found.");
@@ -177,14 +175,11 @@ class Connector {
 
 	/**
 	 * Get the balance of a Discord user.
-	 * @param {Object} data Object containing the user ID to get the balance of.
-	 * @param {String} data.userId User ID to get the balance of.
+	 * @param {String} userId User ID to get the balance of.
 	 * @returns {Object} Balance info about the specified user.
 	 */
-	async setBalance({ userId, balance }) {
-		this.checkInitialized();
-		if (this._disabledEndpoints.includes("setBalance"))
-			throw new Error("Endpoint disabled.");
+	async setBalance(userId, balance) {
+		this.checkEndpoint("setBalance");
 		let updatedRows = await this.db.from("DiscordUser").update({ CurrencyAmount: balance }).where({ UserId: userId });
 		if (updatedRows < 1)
 			throw new Error("User not found.");
@@ -195,17 +190,14 @@ class Connector {
 	}
 
 	/**
-	 * Create a transaction for a Discord user
-	 * @param {Object} data Object containing transaction details.
-	 * @param {String} data.userId User ID to create a transaction for
-	 * @param {Number} data.amount Amount added to or subtracted from the user
-	 * @param {String} data.reason Reason for the transaction
-	 * @returns {Object} Transaction info
+	 * Create a transaction for a Discord user.
+	 * @param {String} userId User ID to create a transaction for.
+	 * @param {Number} amount Amount added to or subtracted from the user.
+	 * @param {String} reason Reason for the transaction.
+	 * @returns {Object} Transaction info.
 	 */
-	async createTransaction({ userId, amount, reason }) {
-		this.checkInitialized();
-		if (this._disabledEndpoints.includes("createTransaction"))
-			throw new Error("Endpoint disabled.");
+	async createTransaction(userId, amount, reason) {
+		this.checkEndpoint("createTransaction");
 		let dateAdded = new Date().toISOString().replace(/[TZ]/g, " ");
 		let row = await this.db.from("CurrencyTransactions").insert({
 			UserId: userId,
@@ -220,17 +212,14 @@ class Connector {
 	}
 
 	/**
-	 * Get transactions of a Discord user
-	 * @param {Object} data Object containing user ID and pagination info
-	 * @param {String} data.userId ID of the user to get transactions of
-	 * @param {Number} data.startPosition Start position/offset of transactions
-	 * @param {Number} data.items Items per page
-	 * @returns {Object} Transactions
+	 * Get transactions of a Discord user.
+	 * @param {String} userId ID of the user to get transactions of.
+	 * @param {Number} startPosition Start position/offset of transactions.
+	 * @param {Number} items Items per page.
+	 * @returns {Object} Transactions.
 	 */
-	async getTransactions({ userId, startPosition, items }) {
-		this.checkInitialized();
-		if (this._disabledEndpoints.includes("getTransactions"))
-			throw new Error("Endpoint disabled.");
+	async getTransactions(userId, startPosition, items) {
+		this.checkEndpoint("getTransactions");
 		let transactions = await this.db.raw(`select Id as 'transactionId', Amount as 'amount', Reason as 'reason', DateAdded as 'dateAdded' from CurrencyTransactions where UserId = ${userId} order by Id desc limit ${items} offset ${startPosition}`);
 		if (!transactions)
 			throw new Error("User not found.");
@@ -241,16 +230,13 @@ class Connector {
 	}
 
 	/**
-	 * Get ranking of a Discord user in a specific guild
-	 * @param {Object} data Object containing user ID and guild ID
-	 * @param {String} data.userId ID of the user to get guild ranking of
-	 * @param {String} data.guildId ID of the guild to get rankings of
-	 * @returns {Object} Rank info
+	 * Get ranking of a Discord user in a specific guild.
+	 * @param {String} userId ID of the user to get guild ranking of.
+	 * @param {String} guildId ID of the guild to get rankings of.
+	 * @returns {Object} Rank info.
 	 */
-	async getGuildRank({ userId, guildId }) {
-		this.checkInitialized();
-		if (this._disabledEndpoints.includes("getGuildRank"))
-			throw new Error("Endpoint disabled.");
+	async getGuildRank(userId, guildId) {
+		this.checkEndpoint("getGuildRank");
 		let guildRankings = await this.db.raw(`select cast(UserId as text) as 'id' from UserXpStats where GuildId=${guildId} order by Xp+AwardedXp desc`).map(user => user.id);
 		if (!guildRankings)
 			throw new Error("Guild not found.");
@@ -261,15 +247,12 @@ class Connector {
 	}
 
 	/**
-	 * Get global ranking of a Discord user
-	 * @param {Object} data Object containing user ID
-	 * @param {String} data.userId ID of the user to get global ranking of
-	 * @returns {Object} Rank info
+	 * Get global ranking of a Discord user.
+	 * @param {String} userId ID of the user to get global ranking of.
+	 * @returns {Object} Rank info.
 	 */
-	async getGlobalRank({ userId }) {
-		this.checkInitialized();
-		if (this._disabledEndpoints.includes("getGlobalRank"))
-			throw new Error("Endpoint disabled.");
+	async getGlobalRank(userId) {
+		this.checkEndpoint("getGlobalRank");
 		let globalRankings = await this.db.raw("select cast(UserId as text) as 'id' from UserXpStats group by UserId order by sum(Xp) desc").map(user => user.id);
 		let rank = await globalRankings.indexOf(userId);
 		if (rank < 0)
@@ -278,16 +261,13 @@ class Connector {
 	}
 
 	/**
-	 * Get the guild XP of a Discord user
-	 * @param {Object} data Object containing user ID and guild ID to get XP stats of.
-	 * @param {String} data.userId ID of the Discord user
-	 * @param {String} data.guildId ID of the Discord server
+	 * Get the guild XP of a Discord user.
+	 * @param {String} userId ID of the Discord user.
+	 * @param {String} guildId ID of the Discord server.
 	 * @returns {Object} Information about the user's XP.
 	 */
-	async getGuildXp({ userId, guildId }) {
-		this.checkInitialized();
-		if (this._disabledEndpoints.includes("getGuildXp"))
-			throw new Error("Endpoint disabled.");
+	async getGuildXp(userId, guildId) {
+		this.checkEndpoint("getGuildXp");
 		let xpInfo = await this.db.first("Xp", "AwardedXp").from("UserXpStats").where({
 			UserId: userId,
 			GuildId: guildId
@@ -308,18 +288,15 @@ class Connector {
 	}
 
 	/**
-	 * Set the guild XP of a Discord user
-	 * @param {Object} data Object containing user ID and guild ID to set XP of.
-	 * @param {String} data.userId ID of the Discord user
-	 * @param {String} data.guildId ID of the Discord server
-	 * @param {String} data.xp XP of the Discord server
-	 * @param {String} data.awardedXp XP awarded to the Discord server
+	 * Set the guild XP of a Discord user.
+	 * @param {String} userId ID of the Discord user.
+	 * @param {String} guildId ID of the Discord server.
+	 * @param {String} xp XP of the Discord user.
+	 * @param {String} awardedXp XP awarded to the Discord user.
 	 * @returns {Object} Information about the user's XP.
 	 */
-	async setGuildXp({ userId, guildId, xp, awardedXp }) {
-		this.checkInitialized();
-		if (this._disabledEndpoints.includes("setGuildXp"))
-			throw new Error("Endpoint disabled.");
+	async setGuildXp(userId, guildId, xp, awardedXp) {
+		this.checkEndpoint("setGuildXp");
 		let updatedRows = await this.db.from("UserXpStats").update({ Xp: xp, AwardedXp: awardedXp }).where({ UserId: userId, GuildId: guildId });
 		if (updatedRows < 1)
 			throw new Error("User/guild not found.");
